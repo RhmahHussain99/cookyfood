60c6cccf51c1d96c3724ad25ea8869613d96d57a
Rahmah <rhmahhussin1420@gmail.com>
2024-01-22 03:50:47 +0300

    second commit

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import AppNavigation from "./src/navigation";
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
-  return (
-    <AppNavigation />
-  );
+	return <AppNavigator />;
 }
 
 
--- src/Screens/WelcomeScreen.js
+++ src/Screens/WelcomeScreen.js
@@ -1,35 +1,50 @@
-import { View, Text } from "react-native";
-import React, { useRef } from "react";
-import { StatusBar } from "expo-status-bar";
-import {
-    widthPercentageToDP as wp,
-    heightPercentageToDP as hp,
-} from "react-native-responsive-screen";
-import { useNavigation } from "@react-navigation/native";
-import LottieView from "lottie-react-native";
-import Animated from "react-native-reanimated";
-const images= require("../../assets/images/background.png")
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React from "react";
+const WelcomeScreen = ({ navigation }) => {
+	return (
+<View style={{ flex: 1, alignItems: "center" }}>
+<Image
+    source={require("../../assets/images/welcome1.png")}/>
+
+<Text style={{ color: "#f96163", fontSize: 22, fontWeight: "bold" }}>
+				40K+ Premium Recipes
+			</Text>
+
+            <Text
+				style={{
+					fontSize: 42,
+					fontWeight: "bold",
+					color: "#3c444c",
+					marginTop: 44,
+					marginBottom: 40,
+				}}
+			>
+				Cook Like A Chef
+			</Text>
+
+            <TouchableOpacity
+				onPress={() => navigation.navigate("RecipeList")}
+				style={{
+					backgroundColor: "#f96163",
+					borderRadius: 18,
+					paddingVertical: 18,
+					width: "80%",
+					alignItems: "center",
+				}}
+			>
+				<Text style={{ fontSize: 18, color: "#fff", fontWeight: "700" }}>
+					Let's Go
+				</Text>
+			</TouchableOpacity>
+
+</View>
 
 
-
-
-
-export default function WelcomeScreen() {
-    const animation = useRef(null);
-  const navigation = useNavigation();
-
-    return(
-        <View>
-            <Image
-        source={images}
-        style={{ 
-          position: "absolute",
-          width: wp(100),
-          height: hp(100),
-          resizeMode: "cover",
-        }}
-      />
-
-        </View>
     );
-}
+    };
+    
+
+
+    export default WelcomeScreen;
+    
+    const styles = StyleSheet.create({});
